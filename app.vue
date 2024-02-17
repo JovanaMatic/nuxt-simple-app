dc5f45e79aeae1b9459661f0c2ae66c6bb21f40e
Jovana Matic <jovana.stankovic85@yahoo.com>
2024-02-17 12:04:24 +0100

    Adding nav heading and card components

--- app.vue
+++ app.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <Nav />
+  <Heading />
+  <Cards />
 </template>
+
+<style>
+  html {
+    font-family: Arial, Helvetica, sans-serif;
+  }
+
+  body {
+    padding: 0;
+    margin: 0;
+  }
+</style>
---
+++ components/Card.vue
@@ -0,0 +1,50 @@
+
+import type { setBlockTracking } from 'vue';
+
+import type { Card } from '#build/components';
+
+import type { Card } from '#build/components';
+<script setup lang="ts">
+  interface CardProps {
+    card: {
+      img: string;
+      title: string;
+      author: string;
+    }
+  }
+
+  const props = defineProps<CardProps>()
+</script>
+
+<template>
+  <div class="card">
+    <img :src="card.img" alt="" />
+    <h3>{{ card.title }}</h3>
+    <p>{{ card.author }}</p>
+
+  </div>
+</template>
+
+<style scoped>
+  .card {
+    box-shadow: 1px 1px 5px black;
+    background: white;
+    width: 30%;
+    margin-bottom: 30px;
+    border-radius: 5px;
+    overflow: hidden;
+  }
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+
+  .content {
+    padding: 10px;
+  }
+
+  p {
+    margin-top: 10px;
+  }
+</style>
